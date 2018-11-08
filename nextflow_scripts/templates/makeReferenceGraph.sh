74f472baf3284bdb622b260fb2d0ecb282bd7a22
Dave Istanto <daveistanto@Daves-MacBook-Pro.local>
2018-11-08 14:30:47 -0600

    make ref graph bug again

--- nextflow_scripts/templates/makeReferenceGraph.sh
+++ nextflow_scripts/templates/makeReferenceGraph.sh
@@ -6,7 +6,7 @@
 
 # Cortex command
 
-${params.cortexDirStep4} ${params.cortexConfigMakeReferenceGraph} --sample_id reference --se_list ${params.pathToReferenceList} --dump_binary ${params.resultsDir}/referenceGraphOutput/ref.ctx > makeReferenceGraph.log
+${params.cortexDirMakeReferenceGraph} ${params.cortexConfigMakeReferenceGraph} --sample_id reference --se_list ${params.pathToReferenceList} --dump_binary ${params.resultsDir}/referenceGraphOutput/ref.ctx > makeReferenceGraph.log
 
 
 

f286a57c1cf8ece9966c3a66f5010cf6d3647caa
Dave Istanto <daveistanto@wirelessprv-10-192-241-147.near.illinois.edu>
2018-11-16 00:44:42 -0600

    basename

--- nextflow_scripts/templates/PDVariantCalling.sh
+++ nextflow_scripts/templates/PDVariantCalling.sh
@@ -4,9 +4,10 @@
 # Script to run step 6 PATH DIVERGENCE Cortex #
 ######################################################################################################################################################################################
 
+echo ${combinationGraph}
 filePath=${combinationGraph}
 basename=\${filePath##*/}
-
+echo \${basename}
 # Cortex command
 
 ${params.cortexDirVariantCalling} ${params.cortexConfigVariantCalling} --max_var_len 50000 --multicolour_bin ${params.resultsDir}/makeCombinationGraphOutput/finalCombinationGraph.ctx --path_divergence_caller [1  --ref_colour 0 --list_ref_fasta ${params.sampleDir}/referenceSoybeanList --path_divergence_caller_output ${params.resultsDir}/variantCallingOutput/${combinationGraph} --print_colour_coverages > \${basename}_PD.log

45616cdf15f8925b2c4cf521dba71fd8b66de733
Dave Istanto <daveistanto@wirelessprv-10-192-241-147.near.illinois.edu>
2018-11-16 00:35:37 -0600

    weird stuff

--- nextflow_scripts/templates/PDVariantCalling.sh
+++ nextflow_scripts/templates/PDVariantCalling.sh
@@ -9,4 +9,4 @@ basename=\${filePath##*/}
 
 # Cortex command
 
-${params.cortexDirVariantCalling} ${params.cortexConfigVariantCalling} --max_var_len 50000 --multicolour_bin ${params.resultsDir}/makeCombinationGraphOutput/finalCombinationGraph.ctx --path_divergence_caller [1  --ref_colour 0 --list_ref_fasta ${params.sampleDir}/referenceSoybeanList --path_divergence_caller_output ${params.resultsDir}/variantCallingOutput/${combinationGraph} --print_colour_coverages > ${combinationGraph}_PD.log
+${params.cortexDirVariantCalling} ${params.cortexConfigVariantCalling} --max_var_len 50000 --multicolour_bin ${params.resultsDir}/makeCombinationGraphOutput/finalCombinationGraph.ctx --path_divergence_caller [1  --ref_colour 0 --list_ref_fasta ${params.sampleDir}/referenceSoybeanList --path_divergence_caller_output ${params.resultsDir}/variantCallingOutput/${combinationGraph} --print_colour_coverages > \${basename}_PD.log
